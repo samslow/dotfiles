3dd6cc3503c7a39b6749a72f56b167b4799a2218
samslow <samslow@tossinvest.com>
2022-02-23 16:01:16 +0900

    fix: m1 brew setting 추가
    Signed-off-by: samslow <samslow@tossinvest.com>

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,5 +1,7 @@
 # Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/samslow/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install yarn
 brew install gh
