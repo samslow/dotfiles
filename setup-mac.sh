cd9bac1bcc68af2ab614e930ae9f464e7f09b210
samslow <samslow@tossinvest.com>
2022-02-23 15:58:38 +0900

    쉘로 세상을 더 편하게
    Signed-off-by: samslow <samslow@tossinvest.com>

---
+++ setup-mac.sh
@@ -0,0 +1,27 @@
+# Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install yarn
+brew install gh
+brew install pure
+brew install n
+
+# ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Brew Cask
+brew install --cask iterm2
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+brew install --cask clipy
+brew install --cask postman
+brew install --cask macs-fan-control
+brew install --cask bettertouchtool
+brew install --cask sourcetree
+brew install --cask notion
+
+# Git
+git config --global user.name "samslow"
+git config --global user.email "samslow@tossinvest.com"
