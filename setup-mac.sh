943d3dd71e45a8dbcb16c311c3888968e901b084
서현석 <samslow@tossinvest.com>
2022-03-24 11:24:57 +0900

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -10,8 +10,6 @@ brew install n
 
 # ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Brew Cask
 brew install --cask iterm2
